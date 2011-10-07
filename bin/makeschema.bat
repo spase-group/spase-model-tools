20602a9b596ab6829797504322c80c340ee137b6
Todd King <tking@igpp.ucla.edu>
2011-10-07 12:37:31 -0700

    Add generation of reference document for data model.

--- bin/makeschema.bat
+++ bin/makeschema.bat
@@ -1,12 +1,18 @@
 :: Make the schema documenation for a version of the SPASE data model.
 :: Using Oxygen schemaDocumentation tool.
 ::
+:: %1 : version
+:: %2 : Path to root of web site output
+::
 :: Author: Todd King
 ::
 copy %2\data\schema\spase-%1.xsd . 
 call "C:\Program Files (x86)\Oxygen XML Editor 12\schemaDocumentation.bat" spase-%1.xsd -out:spase-%1.pdf -format:pdf
 copy spase-%1.pdf %2\data\model\spase-%1.pdf
 copy spase-%1.pdf %2\docs\model\spase-%1.pdf
+
+call "C:\Program Files (x86)\Oxygen XML Editor 12\schemaDocumentation.bat" spase-%1.xsd -cfg:oxygen-html-settings.xml
+move spase-out %2\data\reference\spase-%1
 
 :: Clean-up
 del spase-%1.xsd
--- bin/makeschool.sh
+++ bin/makeschool.sh
@@ -7,7 +7,7 @@ homepath=${2:-/c/projects/spase/webapp/root}
 vername=`echo $version | sed 's/\./_/g'`
 verpack=`echo $version | sed 's/\.//g'`
 
-cp $homepath/docs/dictionary/spase-$vername.pdf $homepath/../school/html/docs/dictionary
-cp $homepath/docs/model/spase-$vername.pdf $homepath/../school/html/docs/model
-cp $homepath/docs/schema/spase-$vername.xsd $homepath/../school/html/docs/schema
-cp $homepath/docs/xmi/spase-$vername.xmi $homepath/../school/html/docs/xmi
+cp $homepath/docs/dictionary/spase-$vername.pdf $homepath/../school/docs/dictionary
+cp $homepath/docs/model/spase-$vername.pdf $homepath/../school/docs/model
+cp $homepath/docs/schema/spase-$vername.xsd $homepath/../school/docs/schema
+cp $homepath/docs/xmi/spase-$vername.xmi $homepath/../school/docs/xmi
